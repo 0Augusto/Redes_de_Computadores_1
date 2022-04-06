74e6c7bc4b1152d690e5a5982549c180a2030943
Henrique Augusto <73003917+0Augusto@users.noreply.github.com>
2022-04-06 11:19:46 -0300

    Add files via upload

---
+++ exemploClienteServidor.java
@@ -0,0 +1,300 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+//Para adotar todos os comprtamentos dessa classe
+public class Servidor extends Thread {
+
+//Declaração dos atributos estáticos para o servidor
+private static ArrayList<BufferedWriter>clientes;
+private static ServerSocket server;
+private String nome;
+private Socket con;
+private InputStream in;
+private InputStreamReader inr;
+private BufferedReader bfr;
+
+/**
+  * Método construtor
+  * @param com do tipo Socket
+  */
+public Servidor(Socket con){
+   this.con = con;
+   try {
+         in  = con.getInputStream();
+         inr = new InputStreamReader(in);
+          bfr = new BufferedReader(inr);
+   } catch (IOException e) {
+          e.printStackTrace();
+   }
+}//end método construtor
+
+/**
+  * Método run
+  */
+public void run(){
+
+  try{
+
+    String msg;
+    OutputStream ou =  this.con.getOutputStream();
+    Writer ouw = new OutputStreamWriter(ou);
+    BufferedWriter bfw = new BufferedWriter(ouw);
+    clientes.add(bfw);
+    nome = msg = bfr.readLine();
+
+    while(!"Sair".equalsIgnoreCase(msg) && msg != null)
+      {
+       msg = bfr.readLine();
+       sendToAll(bfw, msg);
+       System.out.println(msg);
+       }
+
+   }catch (Exception e) {
+     e.printStackTrace();
+
+   }
+}//end método run
+/***
+ * Método usado para enviar mensagem para todos os clients
+ * @param bwSaida do tipo BufferedWriter
+ * @param msg do tipo String
+ * @throws IOException
+ */
+public void sendToAll(BufferedWriter bwSaida, String msg) throws  IOException
+{
+  BufferedWriter bwS;
+
+  for(BufferedWriter bw : clientes){
+   bwS = (BufferedWriter)bw;
+   if(!(bwSaida == bwS)){
+     bw.write(nome + " -> " + msg+"\r\n");
+     bw.flush();
+   }
+  }
+}//end método sendToAll
+/***
+   * Método main
+   * @param args
+   */
+public static void main(String []args) {
+
+  try{
+    //Cria os objetos necessário para instânciar o servidor
+    JLabel lblMessage = new JLabel("Porta do Servidor:");
+    JTextField txtPorta = new JTextField("12345");
+    Object[] texts = {lblMessage, txtPorta };
+    JOptionPane.showMessageDialog(null, texts);
+    server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+    clientes = new ArrayList<BufferedWriter>();
+    JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+
+    txtPorta.getText());
+
+     while(true){
+       System.out.println("Aguardando conexão...");
+       Socket con = server.accept();
+       System.out.println("Cliente conectado...");
+       Thread t = new Servidor(con);
+        t.start();
+    }
+
+  }catch (Exception e) {
+
+    e.printStackTrace();
+  }
+ }// Fim do método main
+} //Fim da classe
+}//end classe servidor
+
+//Cliente
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+import javax.swing.*;
+
+public class Cliente extends JFrame implements ActionListener, KeyListener {//Declarando atributos
+private static final long serialVersionUID = 1L;
+private JTextArea texto;
+private JTextField txtMsg;
+private JButton btnSend;
+private JButton btnSair;
+private JLabel lblHistorico;
+private JLabel lblMsg;
+private JPanel pnlContent;
+private Socket socket;
+private OutputStream ou ;
+private Writer ouw;
+private BufferedWriter bfw;
+private JTextField txtIP;
+private JTextField txtPorta;
+private JTextField txtNome;
+//Método Construtor Cliente
+public Cliente() throws IOException{
+    JLabel lblMessage = new JLabel("Verificar!");
+    txtIP = new JTextField("127.0.0.1");
+    txtPorta = new JTextField("12345");
+    txtNome = new JTextField("Cliente");
+    Object[] texts = {lblMessage, txtIP, txtPorta, txtNome };
+    JOptionPane.showMessageDialog(null, texts);
+     pnlContent = new JPanel();
+     texto              = new JTextArea(10,20);
+     texto.setEditable(false);
+     texto.setBackground(new Color(240,240,240));
+     txtMsg                       = new JTextField(20);
+     lblHistorico     = new JLabel("Histórico");
+     lblMsg        = new JLabel("Mensagem");
+     btnSend                     = new JButton("Enviar");
+     btnSend.setToolTipText("Enviar Mensagem");
+     btnSair           = new JButton("Sair");
+     btnSair.setToolTipText("Sair do Chat");
+     btnSend.addActionListener(this);
+     btnSair.addActionListener(this);
+     btnSend.addKeyListener(this);
+     txtMsg.addKeyListener(this);
+     JScrollPane scroll = new JScrollPane(texto);
+     texto.setLineWrap(true);
+     pnlContent.add(lblHistorico);
+     pnlContent.add(scroll);
+     pnlContent.add(lblMsg);
+     pnlContent.add(txtMsg);
+     pnlContent.add(btnSair);
+     pnlContent.add(btnSend);
+     pnlContent.setBackground(Color.LIGHT_GRAY);
+     texto.setBorder(BorderFactory.createEtchedBorder(Color.BLUE,Colo  r.BLUE));
+     txtMsg.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
+     setTitle(txtNome.getText());
+     setContentPane(pnlContent);
+     setLocationRelativeTo(null);
+     setResizable(false);
+     setSize(250,300);
+     setVisible(true);
+     setDefaultCloseOperation(EXIT_ON_CLOSE);
+}//end construtor Cliente
+//Conectar
+/***
+  * Método usado para conectar no server socket, retorna IO Exception caso dê algum erro.
+  * @throws IOException
+  */
+public void conectar() throws IOException{
+
+  socket = new Socket(txtIP.getText(),Integer.parseInt(txtPorta.getText()));
+  ou = socket.getOutputStream();
+  ouw = new OutputStreamWriter(ou);
+  bfw = new BufferedWriter(ouw);
+  bfw.write(txtNome.getText()+"\r\n");
+  bfw.flush();
+}//end Conectar
+/***
+  * Método usado para enviar mensagem para o server socket
+  * @param msg do tipo String
+  * @throws IOException retorna IO Exception caso dê algum erro.
+  */
+  public void enviarMensagem(String msg) throws IOException{
+
+    if(msg.equals("Sair")){
+      bfw.write("Desconectado \r\n");
+      texto.append("Desconectado \r\n");
+    }else{
+      bfw.write(msg+"\r\n");
+      texto.append( txtNome.getText() + " diz -> " +         txtMsg.getText()+"\r\n");
+    }
+     bfw.flush();
+     txtMsg.setText("");
+}//end envia mensagem
+/**
+ * Método usado para receber mensagem do servidor
+ * @throws IOException retorna IO Exception caso dê algum erro.
+ */
+public void escutar() throws IOException{
+
+   InputStream in = socket.getInputStream();
+   InputStreamReader inr = new InputStreamReader(in);
+   BufferedReader bfr = new BufferedReader(inr);
+   String msg = "";
+
+    while(!"Sair".equalsIgnoreCase(msg))
+
+       if(bfr.ready()){
+         msg = bfr.readLine();
+       if(msg.equals("Sair"))
+         texto.append("Servidor caiu! \r\n");
+        else
+         texto.append(msg+"\r\n");
+        }
+}//end Escutar
+/***
+  * Método usado quando o usuário clica em sair
+  * @throws IOException retorna IO Exception caso dê algum erro.
+  */
+  public void sair() throws IOException{
+
+   enviarMensagem("Sair");
+   bfw.close();
+   ouw.close();
+   ou.close();
+   socket.close();
+}//end sair
+@Override
+public void actionPerformed(ActionEvent e) {
+
+  try {
+     if(e.getActionCommand().equals(btnSend.getActionCommand()))
+        enviarMensagem(txtMsg.getText());
+     else
+        if(e.getActionCommand().equals(btnSair.getActionCommand()))
+        sair();
+     } catch (IOException e1) {
+          // TODO Auto-generated catch block
+          e1.printStackTrace();
+     }
+}//end actionPerformed
+@Override
+public void keyPressed(KeyEvent e) {
+
+    if(e.getKeyCode() == KeyEvent.VK_ENTER){
+       try {
+          enviarMensagem(txtMsg.getText());
+       } catch (IOException e1) {
+           // TODO Auto-generated catch block
+           e1.printStackTrace();
+       }
+   }
+}
+
+@Override
+public void keyReleased(KeyEvent arg0) {
+  // TODO Auto-generated method stub
+}
+
+@Override
+public void keyTyped(KeyEvent arg0) {
+  // TODO Auto-generated method stub
+}
+public static void main(String []args) throws IOException{
+
+   Cliente app = new Cliente();
+   app.conectar();
+   app.escutar();
+}//end main
